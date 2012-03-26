c3a3e70f1385c89d3d8f19d98bc9d38154f50bef
corinne <corinnekrych@gmail.com>
2012-03-26 23:19:57 +0200

    integrating dust

--- web-app/js/EventList.js
+++ web-app/js/EventList.js
@@ -13,7 +13,20 @@ EventList.prototype.get = function(index) {
 EventList.prototype.renderToHtml = function() {
 	for ( var i = 0; i < this.events.length; i++) {
 		var event = this.events[i];
-		$("#contentToAdd").append('<li><a href="event-edit.html" data-rel="dialog" data-transition="fade">' + event.name + '</a></li>');
+		$("#contentToAdd").append(
+				'<li><a href="event-edit.html" data-rel="dialog" data-transition="fade">'
+						+ event.name + '</a></li>');
 	}
 	$('#contentToAdd').listview('refresh');
+};
+
+EventList.prototype.renderToDustTemplate = function() {
+	// {"date":"2012-03-25T22:00:00Z","id":1,"name":"ouiiiiiiii"}
+	var template = '<li><a href="event-edit.html" data-rel="dialog" data-transition="fade">{#events}{name}{/events}</a></li>';
+	var compiled = dust.compile(template, "intro");
+	dust.loadSource(compiled);
+	dust.render("intro", this.events, function(err, out) {
+		$("#contentToAdd").append(out);
+		$('#contentToAdd').listview('refresh');
+	});
 };
