f27a4e5cb230451c6586db4ca33f777e2782faf8
Fabrice Matrat <fabricematrat@gmail.com>
2012-04-14 12:05:54 +0200

    js validation trial

--- web-app/js/Event.js
+++ web-app/js/Event.js
@@ -11,6 +11,8 @@ function showEvent(id) {
 	var event = $("#section-events").data("Event_" + id);
 	$('#input-event-name').val(event.name);
 	$('#field-event-name').fieldcontain('refresh');
+	$('#input-event-date').val(event.date);
+	$('#field-event-date').fieldcontain('refresh');
 }
 
 Event.prototype.renderToHtml = function() {
@@ -27,3 +29,17 @@ function getUrlVars() {
     }
     return vars;
 }
+
+
+
+$('#form-update-event').submit(function() {
+	$('#form-update-event').validate();
+    
+    // Submit the form
+    $.post("/forms/requestProcessor.php", form1Var.serialize(), function(data){
+      confirmationVar.text(data);
+      hideContentTransition();
+      showConfirmation();
+    });        
+    return false;      
+});    
